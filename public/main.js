fa320e174ab2dd1e3dce55602b00b0cc9c52860c
Hoshiki Tsunoda <liquidohr@gmail.com>
2017-10-30 11:29:52 -0700

    create a function to find words based on the letter typed in. #1

--- public/main.js
+++ public/main.js
@@ -10,3 +10,13 @@ getData()
     names.push(people.name)
   }))
   .catch(err => console.error(err))
+
+const suggest = (list, char) => {
+  const newSug = []
+  for (let i = 0; i < list.length; i++) {
+    if (list[i].slice(0, char.length) === char) {
+      newSug.push(list[i])
+    }
+  }
+  return newSug
+}
