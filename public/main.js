7ea8dc412aebd30eca4c8134a8ff376672b0066d
Hoshiki Tsunoda <liquidohr@gmail.com>
2017-10-30 15:07:46 -0700

    Append it to input so it displays the result. #1

--- public/main.js
+++ public/main.js
@@ -7,30 +7,46 @@ const names = []
 
 getData()
   .then((data) => data.forEach(people => {
-    names.push(people.name)
+    names.push(people.name.toLowerCase())
   }))
   .catch(err => console.error(err))
 
 const suggest = (list, char) => {
   const newSug = []
   for (let i = 0; i < list.length; i++) {
-    if (list[i].slice(0, char.length) === char) {
+    if (list[i].startsWith(char)) {
       newSug.push(list[i])
     }
   }
+  console.log(newSug);
   return newSug
 }
 
+const $input = document.querySelector('input')
+const $ul = document.querySelector('ul')
+const $wrapper = document.querySelector('#wrapper')
+
+const capitalize = (str) => {
+  return str
+    .split(' ')
+    .map((name) => {
+      return name[0].toUpperCase() + name.substr(1)
+    })
+    .join(' ')
+}
+
 $input.addEventListener('keyup', (event) => {
-  const char = event.key
-  const result = suggest(names, char)
-  if (char === 'Backspace') {
+  const input = event.target.value
+  console.log(input);
+  if (input) {
     $ul.innerHTML = ''
-  }
-  else {
-    for (let i = 0; i < result.length; i++) {
-      const suggestion = "<li>" + result[i] + "</li>"
-      $ul.innerHTML += suggestion
+    const suggestion = suggest(names, input)
+    for (let i = 0; i < suggestion.length; i++) {
+      $ul.innerHTML += "<li>" + capitalize(suggestion[i]) + "</li>"
     }
   }
+  else {
+    $ul.innerHTML = ''
+  }
+  $wrapper.appendChild($ul)
 })
